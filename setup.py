e118f6c9bb21fc15ab98d1a188cb8f758892e30d
Tim Empringham <tim.empringham@live.ca>
2020-07-06 21:55:10 -0400

    Fixed setup.py for release

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
+
 setup(
   name = 'omnilogic',
   packages = ['omnilogic'],
-  version = '0.2.2',
+  version = '0.2.3',
   license='apache-2.0',
   description = 'Integration for the Hayward OmniLogic pool control system',
   author = 'Tim Empringham',
   author_email = 'tim.empringham@live.ca',
   url = 'https://github.com/djtimca/omnilogic',
-  download_url = 'https://github.com/djtimca/omnilogic/archive/v_022.tar.gz',
+  download_url = 'https://github.com/djtimca/omnilogic/archive/v_023.tar.gz',
   keywords = ['OmniLogic', 'Hayward', 'Pool', 'Spa'],
   install_requires=[
           'xmltodict',
