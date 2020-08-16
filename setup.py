b24edae277a67872571b2677b8bc5dd85e0390db
Tim Empringham <tim.empringham@live.ca>
2020-08-16 09:39:55 -0400

    Bumped release to 0.3.5 for PyPi push.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'omnilogic',
   packages = ['omnilogic'],
-  version = '0.3.4',
+  version = '0.3.5',
   license='apache-2.0',
   description = 'Integration for the Hayward OmniLogic pool control system',
   author = 'Tim Empringham',
   author_email = 'tim.empringham@live.ca',
   url = 'https://github.com/djtimca/omnilogic',
-  download_url = 'https://github.com/djtimca/omnilogic/archive/v_034.tar.gz',
+  download_url = 'https://github.com/djtimca/omnilogic/archive/v_035.tar.gz',
   keywords = ['OmniLogic', 'Hayward', 'Pool', 'Spa'],
   install_requires=[
           'xmltodict',
